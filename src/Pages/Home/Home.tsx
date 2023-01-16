f621e7b7f405c4192032a181fb34920cb1a306b7
Dawid <dawid.rozak.dev@gmail.com>
2023-01-16 21:13:47 +0100

    16.01.2022 - Add sections in Home page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Jumbutron from '../../components/Jumbutron/Jumbutron'
 import FeatureBox from '../../components/FeatureBox/FeatureBox'
+import Advantages from '../../components/Advantages/Advantages'
+import { Flex, Heading, VStack } from '@chakra-ui/react'
 
 const Home = () => {
     return(
         <>
             <Jumbutron />
+            <Flex flexDir="column">
             <FeatureBox />
-
+            <Heading textAlign="center" mb="42px">Zapisuj swoje obserwacje </Heading>
+            <Advantages />
+            </Flex>
         </>
     )
 }
